46429a36d7553f79bc317f9082d66a563296c947
Dravin Chauhan <86051188+N-Dravin@users.noreply.github.com>
2021-09-06 17:05:52 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class First
+{
+  public static void main(String[] args)
+  {
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter Full Name");
+    String str = s.nextLine();
+  }
+}
